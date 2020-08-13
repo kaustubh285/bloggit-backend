16a797646e3545dff6c846307a6f6e7b006628fd
kaustubh <kaustubh285@gmail.com>
2020-08-13 14:54:11 +0530

    Upvoting, and comments can be added to database using api end points

--- src/server.js
+++ src/server.js
@@ -6,78 +6,84 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// const articleInfo = {
-//   "learn-react": {
-//     upvotes: 0,
-//     comments: [],
-//   },
-//   "learn-node": {
-//     upvotes: 0,
-//     comments: [],
-//   },
-//   "my-thoughts-on-resumes": {
-//     upvotes: 0,
-//     comments: [],
-//   },
-// };
-
-app.get("/api/article/:name", async (req, res) => {
+const withDB = async (operations, res) => {
   try {
-    const articleName = req.params.name;
     const client = await MongoClient.connect("mongodb://127.0.0.1:27017/", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     const db = client.db("mybloggit");
 
+    await operations(db);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong", error });
+  }
+};
+
+// GET req to fetch article details
+app.get("/api/article/:name", async (req, res) => {
+  withDB(async (db) => {
+    const articleName = req.params.name;
+
     const articleInfo = await db
       .collection("articles")
       .findOne({ name: articleName });
 
     res.status(200).json(articleInfo);
-    client.close();
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong", error });
-  }
+  }, res);
 });
 
+// POST req to increase upvotes for an article
 app.post("/api/article/:name/upvote", async (req, res) => {
-  const articleName = req.params.name;
+  withDB(async (db) => {
+    const articleName = req.params.name;
 
-  const client = await MongoClient.connect("mongodb://127.0.0.1:27017/", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
 
-  const db = client.db("mybloggit");
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          upvotes: articleInfo.upvotes + 1,
+        },
+      }
+    );
 
-  const articleInfo = await db
-    .collection("articles")
-    .findOne({ name: articleName });
+    const updatedArticleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
 
-  await db.collection("articles").updateOne(
-    { name: articleName },
-    {
-      $set: {
-        upvotes: articleInfo.upvotes + 1,
-      },
-    }
-  );
-
-  const updatedArticleInfo = await db
-    .collection("articles")
-    .findOne({ name: articleName });
-
-  res.status(200).json(updatedArticleInfo);
-  client.close();
+    res.status(200).json(updatedArticleInfo);
+  }, res);
 });
 
 app.post("/api/article/:name/add-comment", (req, res) => {
-  const { username, text } = req.body;
-  const articleName = req.params.name;
+  withDB(async (db) => {
+    const articleName = req.params.name;
+    const { username, text } = req.body;
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
 
-  articleInfo[articleName].comments.push({ username, text });
-  res.status(200).send(articleInfo[articleName]);
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          comments: articleInfo.comments.concat({ username, text }),
+        },
+      }
+    );
+
+    const updatedArticleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+  }, res);
 });
 
 app.listen(8001, () => console.log("Listening on post 8001"));
